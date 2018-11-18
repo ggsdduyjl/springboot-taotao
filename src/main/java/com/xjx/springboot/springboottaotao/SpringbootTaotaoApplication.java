e0d9bf6245f3d28103b3ac22955dfa421b19b55c
xjxxxj <1690091317@qq.com>
2018-11-19 00:44:54 +0800

    springboot-taotao项目初始化

---
+++ src/main/java/com/xjx/springboot/springboottaotao/SpringbootTaotaoApplication.java
@@ -0,0 +1,12 @@
+package com.xjx.springboot.springboottaotao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTaotaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTaotaoApplication.class, args);
+    }
+}
